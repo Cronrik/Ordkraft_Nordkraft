65c1844396843c1134e2a0e462b08cdbb042da44
Henrik Martens <Henrikm87@outlook.dk>
2023-02-23 13:31:08 +0100

    .xck.vfdsv

---
+++ Test_area/Test_area/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_area.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
