ebfa3a40ea52c3599c0b8db92b061143fa90feab
Sigivald <10461972@ucn.dk>
2023-03-01 09:42:41 +0100

    javascript til ikonet i begivenheds elementerne

--- Assets/js/Site.js
+++ Assets/js/Site.js
@@ -1,4 +1,13 @@
+// fang sjerne ikonet fra begivenheds elementerne
+let iconClick = document.getElementById("demo")
 // onClick event til at ændre sjernes farve når man klikker på den
 function myFunction(){
-    document.getElementById("demo").style.color = "red"
+    // laver et if for at checke om ikonets farve er grå
+    if(iconClick.style.color == "grey"){
+        // hvis ikonet er grå, så skifter dem farve til rød
+        iconClick.style.color = "red";
+        // hvis den ikke er grå, så skifter det til grå
+    } else{
+        iconClick.style.color = "grey"
+    }
 }
