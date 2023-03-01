d93c678b90ae9037bb16b7169c0db715c192c037
Sigivald <10461972@ucn.dk>
2023-03-01 12:53:42 +0100

    js ændringer

--- Assets/js/Site.js
+++ Assets/js/Site.js
@@ -40,6 +40,34 @@ function myFunction3(){
     }
 }
 
+// fang sjerne ikonet fra begivenheds elementerne
+let iconClick4 = document.getElementById("star4");
+// onClick event til at ændre sjernes farve når man klikker på den
+function myFunction4(){
+    // laver et if for at checke om ikonets farve er grå
+    if(iconClick4.style.color == "grey"){
+        // hvis ikonet er grå, så skifter dem farve til rød
+        iconClick4.style.color = "red";
+        // hvis den ikke er grå, så skifter det til grå
+    } else{
+        iconClick4.style.color = "grey";
+    }
+}
+
+// fang sjerne ikonet fra begivenheds elementerne
+let iconClick5 = document.getElementById("star5");
+// onClick event til at ændre sjernes farve når man klikker på den
+function myFunction5(){
+    // laver et if for at checke om ikonets farve er grå
+    if(iconClick5.style.color == "grey"){
+        // hvis ikonet er grå, så skifter dem farve til rød
+        iconClick5.style.color = "red";
+        // hvis den ikke er grå, så skifter det til grå
+    } else{
+        iconClick5.style.color = "grey";
+    }
+}
+
 const modal = document.querySelector(".popup");
 const trigger = document.querySelector(".udløs");
 
